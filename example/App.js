0c2e3e8faf0a765ac9b26636fd37ae57ef9d643f
sgj0 <contact@sgj0.eu>
2019-02-09 17:06:15 +0100

    improve react native example

--- example/App.js
+++ example/App.js
@@ -1,15 +1,137 @@
 import React from 'react';
-import Routing from './Routing';
+import {StyleSheet, Text, View, Alert} from 'react-native';
+import {Constants} from 'expo';
+
+import Routing from 'react-native-leaflet-routing';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  // display an alert
+  showAlert = (title, body) => {
+    Alert.alert(title, body, [
+      {
+        text: 'OK',
+        onPress: () => console.log('OK Pressed')
+      }
+    ], {cancelable: false});
+  };
+
   render() {
     const from = [48.87541, 2.32555];
     const to = [48.85513, 2.38713];
+    const urlRouter = 'http://127.0.0.1:5000/route/v1';
+    const eventReceiver = {
+      onLoad: (event) => {
+        console.log('onLoad received : ', event);
+      },
 
-    return (<Routing from={from} to={to}/>);
+      onUnload: (event) => {
+        console.log('onUnload received : ', event);
+      },
+
+      onMapLoaded: (event) => {
+        console.log('onMapLoaded received : ', event);
+      },
+
+      onUpdateMapState: (event) => {
+        console.log('onUpdateMapState received : ', event);
+      },
+
+      onMapClicked: (event) => {
+        console.log('onMapClicked received : ', event);
+        this.showAlert('Map Clicked', `Coordinates = ${event.payload.coords}`);
+      },
+
+      onMapMarkerClicked: (event) => {
+        console.log('onMapMarkerClicked received : ', event);
+      },
+
+      onZoom: (event) => {
+        console.log('onZoom received : ', event);
+      },
+
+      onZoomStart: (event) => {
+        console.log('onZoomEnd received : ', event);
+      },
+
+      onZoomEnd: (event) => {
+        console.log('onZoomEnd received : ', event);
+      },
+
+      onZoomLevelsChange: (event) => {
+        console.log('onZoomLevelsChange received : ', event);
+      },
+
+      onMove: (event) => {
+        console.log('onMove received : ', event);
+      },
+
+      onMoveStart: (event) => {
+        console.log('onMoveStart received : ', event);
+      },
+
+      onMoveEnd: (event) => {
+        console.log('onMoveEnd received : ', event);
+      },
+
+      onCurrentPositionClicked: (event) => {
+        console.log('onCurrentPositionClicked received : ', event);
+      },
+
+      onResize: (event) => {
+        console.log('onResize received : ', event);
+      },
+
+      onViewReset: (event) => {
+        console.log('onViewReset received : ', event);
+      },
+
+      onRoutesFound: (event) => {
+        console.log('onRoutesFound received : ', event);
+      },
+
+      onRouteSelected: (event) => {
+        console.log('onRouteSelected received : ', event);
+      },
+
+      onRouteError: (event) => {
+        console.log('onRouteError received : ', event);
+      }
+    };
+
+    return (<View style={styles.container}>
+      <View style={styles.statusBar}/>
+      <Text style={{
+          margin: 10,
+          fontSize: 18,
+          color: 'black'
+        }}>
+        react-native-leaflet-routing
+      </Text>
+
+      <Routing
+        // required: coordinates of the starting point
+        from={from}
+        // required: coordinates of the arriving point
+        to={to}
+        // optional : event functions
+        eventReceiver={eventReceiver}
+        // optional: url of routing server
+        urlRouter={urlRouter}/>
+    </View>);
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#00ffff',
+    display: 'flex'
+  },
+  statusBar: {
+    height: Constants.statusBarHeight
+  }
+});
